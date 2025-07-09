f2e51f32eb335c57b991738e080064e54ccb1952
Grit <68225058+Grit03@users.noreply.github.com>
2025-07-09 19:22:17 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.33 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -0,0 +1,9 @@
+function solution(intStrs, k, s, l) {
+    const answer = [];
+    for(const str of intStrs){
+        const num = Number(str.slice(s, s+l));
+        if(num > k) answer.push(num); 
+    }
+    
+    return answer;
+}
