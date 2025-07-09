a3a15c03c7e68e421bd90e8c39ec60e78553f2d3
Grit <68225058+Grit03@users.noreply.github.com>
2025-07-09 15:44:08 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,31 +1,30 @@
 
 function solution(a, b, c, d) {
-    // 네 주사위에서 나온 숫자가 모두 p로 같다면 1111 × p점을 얻습니다.
-    if(a === b && b === c && c === d) return 1111 * a;
+    const dices = [a, b, c, d];
+    const set = [...new Set(dices)];
+    const map = new Map();
+    for(const key of set){
+        map.set(key, dices.filter((v) => v === key).length);
+    }
     
-    // 세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가
-    // q(p ≠ q)라면 (10 × p + q)^2 점을 얻습니다.
-    if(b === c && c === d) return (10 * b + a) ** 2;
-    if(a === c && c === d) return (10 * a + b) ** 2;
-    if(a === b && b === d) return (10 * a + c) ** 2;
-    if(a === b && b === c) return (10 * a + d) ** 2;
-    
-    // 주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면
-    // (p + q) × |p - q|점을 얻습니다.
-    if(a === b && c === d) return (a + c) * Math.abs(a - c);
-    if(a === c && b === d) return (a + b) * Math.abs(a - b);
-    if(a === d && b === c) return (a + b) * Math.abs(a - b);
-    
-    // 어느 두 주사위에서 나온 숫자가 p로 같고 나머지 두 주사위에서 나온 숫자가
-    // 각각 p와 다른 q, r(q ≠ r)이라면 q × r점을 얻습니다.
-    if(a === b) return c * d;
-    if(a === c) return b * d;
-    if(a === d) return b * c;
-    if(b === c) return a * d;
-    if(b === d) return a * c;
-    if(c === d) return a * b;
-    
-    return Math.min(a, b, c, d);
-    
-    
+    switch(map.size){
+        case 1 : return 1111 * set[0];
+        case 2 : {
+            // 개수가 같으면
+            if(map.get(set[0]) === map.get(set[1])){
+                const [p, q] = set;
+                return (p + q) * Math.abs(p - q);
+            }else{
+                // 개수가 다르면
+                const [p, q] = map.get(set[0]) > map.get(set[1]) ? set : [set[1], set[0]];
+                return (10 * p + q) ** 2;
+            }
+        }
+        case 3 : {
+            const mapArr = [...map.entries()].sort((a, b) => b[1] - a[1]);
+            console.log(mapArr);
+            return mapArr[1][0] * mapArr[2][0];
+        }
+        case 4 : return set.sort((a, b) => a - b)[0];
+    }
 }
