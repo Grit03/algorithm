96e658f74aef8ac77ca62c987bbe0ae37dd9d278
Grit <68225058+Grit03@users.noreply.github.com>
2025-09-30 13:00:33 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.25 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,14 +1,11 @@
-function addMonth([year, month, day], add) {
-  const total = (month - 1) + add;
-  const newYear = year + Math.floor(total / 12);
-  const newMonth = (total % 12) + 1;
-  return [newYear, newMonth, day];
+function timestamp([year, month, day]) {
+  return ((year * 12 + month) * 28) + day;
 }
 
 function solution(today, terms, privacies) {
-    const [year, month, day] = today.split(".").map(Number);
+    const todayStr = today.split(".").map(Number);
+    const todayTimestamp = timestamp(todayStr);
     
-    console.log(year, month, day);
     
     const termsMap = terms.reduce((acc, cur) => {
         const [term, duration] = cur.split(" ");
@@ -19,14 +16,10 @@ function solution(today, terms, privacies) {
     
     const ans = privacies.reduce((ans, pravicy, idx) => {
         const [dateStr, type] = pravicy.split(" ");
-        console.log(dateStr, type);
-        const date = dateStr.split(".").map(Number);
-        const [dYear, dMonth, dDay] = addMonth(date, termsMap[type]);
-        console.log(`${dYear}, ${dMonth}, ${dDay}`)
+        const [year, month, day] = dateStr.split(".").map(Number);
+        const privacyTimestamp = timestamp([year, month + termsMap[type], day]);
         
-        if(dYear < year) return [...ans, idx+1];
-        if(dYear === year && dMonth < month) return [...ans, idx+1];
-        if(dYear === year && dMonth === month && dDay - 1 < day) return [...ans, idx+1];
+        if(privacyTimestamp <= todayTimestamp) return [...ans, idx+1];
         return ans;
     }, []);
     
