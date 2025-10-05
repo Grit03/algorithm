89e8046c952c9d8934aa10090f3160200830bed9
Grit <68225058+Grit03@users.noreply.github.com>
2025-10-05 12:56:38 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,37 +1,12 @@
-function solution(new_id) {
-    // 1단계
-    let id = new_id.toLowerCase();
-    console.log("1단계:", id);
-    // 2단계
-    id = id.split("").filter((char) => /[a-z0-9-_.]/.test(char)).join("");
-    console.log("2단계:", id);
-    // 3단계
-    id = id.replaceAll(/[.]+/g, ".");
-    console.log("3단계:", id);
-    
-    // 4단계
-    const idArr = id.split("");
-    if(idArr[0] === ".") idArr[0] = "";
-    if(idArr[idArr.length -1] === ".") idArr[idArr.length -1] = "";
-    id = idArr.join("");
-    console.log("4단계:", id);
-    
-    // 5단계
-    if(id === "") id = "a";
-    console.log("5단계:", id);
-    // 6단계
-    if(id.length >= 16){
-        id = id.slice(0, 15);
-        if(id[0] === ".") id = id.slice(1);
-        if(id[id.length -1] === ".") id = id.slice(0, id.length -1);
-    }
-    console.log("6단계:", id);
-    // 7단계
-    if(id.length <=2){
-        const idLength = id.length;
-        id = id + id[idLength - 1].repeat(3 - idLength);
-    }
-    console.log("7단계:", id);
 
-    return id;
+function solution(new_id) {
+    const answer = new_id
+        .toLowerCase() // 1
+        .replace(/[^\w-_.]/g, '') // 2
+        .replace(/\.+/g, '.') // 3
+        .replace(/^\.|\.$/g, '') // 4
+        .replace(/^$/g, 'a') // 5
+        .slice(0, 15).replace(/\.$/, ''); // 6
+    const len = answer.length;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
 }
