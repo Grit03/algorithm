833a011712d293e24edf6b7a6798b1e96c77ec0a
Grit <68225058+Grit03@users.noreply.github.com>
2025-10-11 01:53:44 +0900

    [level 2] Title: 문자열 압축, Time: 3.67 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,48 +1,22 @@
-// 1개 이상의 단위로 잘라서 압축
-// 1개인 경우는 1 생략
-// 1개 이상인 단위로 문자열을 잘라 압축했을 때, 가장 짧은 개수
-
-// 1개~Math.floor(길이/2)
 function solution(s) {
-    const length = s.length;
-    
     let min = Infinity;
-
-    for(let i = 1; i <= length; i++){
-        const charArr = s.split("");
-        let left = length;
-        let prev;
+    for(let len = 1; len <=s.length; len++){
+        let prev = s.slice(0, len);
         let count = 1;
-        let zipped = "";
-        for(let j=0; j <= length; j += i){
-            if(j+i > length){
-                if(!prev) {
-                    zipped = s;
-                    break;
-                }
-                zipped += (count === 1) ? `${prev}` :`${count}${prev}`;
-                left = j;
-                break;
-            }
-            const key = charArr.slice(j, j+i).join("");
-            if(!prev){
-                prev = key;
-                continue;
-            }
-            if(prev === key){
-                count += 1;
+        let result = "";
+        for(let start = len; start <s.length; start +=len){
+            const cur = s.slice(start, start+len);
+            if(prev === cur){
+                count++;
             }else{
-                zipped += (count === 1) ? `${prev}` :`${count}${prev}`;
-                prev = key;
+                result += count === 1 ? prev : `${count}${prev}`;
+                prev = cur;
                 count = 1;
             }
         }
-        const leftStr = charArr.slice(left).join("") ?? "";
-        const result = zipped + leftStr;
+        result += count === 1 ? prev : `${count}${prev}`;
         min = Math.min(min, result.length);
-        
     }
-    
-    return min;
 
+    return min;
 }
