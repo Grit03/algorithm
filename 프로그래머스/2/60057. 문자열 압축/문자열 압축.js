bfa8ab93c803f54336b5778ab50f6b36ba582a30
Grit <68225058+Grit03@users.noreply.github.com>
2026-02-01 14:22:56 +0900

    [level 2] Title: 문자열 압축, Time: 3.76 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,22 +1,22 @@
 function solution(s) {
-    let min = Infinity;
-    for(let len = 1; len <=s.length; len++){
-        let prev = s.slice(0, len);
+    let answer = 9999;
+    if(s.length === 1) return 1;
+    for(let i = 1; i <= Math.floor(s.length /2); i++){
+        let compressed = "";
+        let currentChars = s.slice(0, i);
         let count = 1;
-        let result = "";
-        for(let start = len; start <s.length; start +=len){
-            const cur = s.slice(start, start+len);
-            if(prev === cur){
-                count++;
-            }else{
-                result += count === 1 ? prev : `${count}${prev}`;
-                prev = cur;
+        
+        for(let j = i; j < s.length; j += i){
+            const next = s.slice(j, j+i);
+            if(currentChars === next) count++;
+            else{
+                compressed += `${count <= 1 ? "" : count}${currentChars}`
                 count = 1;
+                currentChars = next;
             }
         }
-        result += count === 1 ? prev : `${count}${prev}`;
-        min = Math.min(min, result.length);
+        compressed += `${count <= 1 ? "" : count}${currentChars}`
+        answer = Math.min(answer, compressed.length);
     }
-
-    return min;
+    return answer;
 }
