e75519d3760d0b056ccebf055e699e77b4330820
Grit <68225058+Grit03@users.noreply.github.com>
2026-02-16 14:55:45 +0900

    [level 0] Title: A로 B 만들기, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,26 +1,3 @@
 function solution(before, after) {
-    // 구성요소와 그 개수가 일치하면 만들 수 있다.
-    const beforeDict = new Map();
-    const afterDict = new Map();
-    for(const char of before){
-        beforeDict.set(char, (beforeDict.get(char) ?? 0) +1);
-    }
-    
-    for(const char of after){
-        afterDict.set(char, (afterDict.get(char) ?? 0) +1);
-    }
-    
-    let flag = 1;
-    for(const [key, count] of beforeDict.entries()){
-        if(!afterDict.get(key)){
-            flag = 0;
-            break;
-        }
-        if(afterDict.get(key) !== count){
-            flag = 0;
-            break;
-        }
-    }
-    
-    return flag;
+    return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0;
 }
